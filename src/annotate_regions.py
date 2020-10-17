c820b93430eb461f34922eae0815f8e0e11aa71d
Tyrone Chen <tyrone.chen@monash.edu>
2020-10-18 00:38:30 +1100

    Rewrite region annotator to be O(n) instead of O(m x n)

--- src/annotate_regions.py
+++ src/annotate_regions.py
@@ -6,6 +6,7 @@ import re
 import subprocess as sp
 from time import time
 import warnings
+from intervaltree import Interval, IntervalTree
 import pandas as pd
 from tqdm import tqdm
 
@@ -56,8 +57,66 @@ def filter_regions(regions: pd.DataFrame, filter_col: str="adj.P.Val",
     """
     return(regions[regions[filter_col] <= filter_val])
 
-def annotate_regions(abundance: pd.DataFrame, gtf: pd.DataFrame,
-                     outfile_path: str="./out.tsv", hide_progress: bool=True):
+def annotate_regions_lowmem(abundance: pd.DataFrame, gtf: pd.DataFrame,
+                            outfile_path: str="./out.tsv",
+                            hide_progress: bool=True):
+    """
+    Annotate region of genome with identifiers. The output format is line-based
+    and multi-mapping is possible, for example if a region of interest contains
+    more than one genomic feature. In these cases, the line will be duplicated!
+    (No good reason to use this slow function, but leaving here in case needed)
+
+    Arguments:
+        (REQUIRED) abundance: abundance measurements in dataframe
+        (REQUIRED) gtf: genome annotations in dataframe
+        (OPTIONAL) outfile_path: path to output file (DEFAULT: ./out.tsv)
+        (OPTIONAL) hide_progress: show the progress bar (DEFAULT: True)
+
+    Example input (abundance):
+
+                   SAMPLE_1     SAMPLE_2    ...
+        FEATURE_1      1123         3402    ...
+        FEATURE_2       110          149    ...
+
+    Example input (gtf):
+
+        seqnames    ...
+        FOO         ...
+        BAR         ...
+
+    Example output:
+
+                   SAMPLE_1     SAMPLE_2   ...  Gene id  ...
+        FEATURE_1      1123         3402   ...     FOO1  ...
+        FEATURE_1      1123         3402   ...     FOO2  ...
+        FEATURE_2       110          149   ...      BAR  ...
+    """
+    for abu_entry in tqdm(abundance.iterrows(), total=abundance.shape[0],
+                          desc="Annotating genomic regions",
+                          disable=hide_progress):
+        abu_start = abu_entry[1]["start_region"]
+        abu_end = abu_entry[1]["end_region"]
+        abu_chr = abu_entry[1]["region_names"]
+
+        for gtf_entry in gtf.iterrows():
+            gtf_start = gtf_entry[1]["start"]
+            gtf_end = gtf_entry[1]["end"]
+            gtf_chr = gtf_entry[1]["seqnames"]
+
+            if abu_chr == gtf_chr:
+                if len(range(max(gtf_start,abu_start),min(gtf_end,abu_end)+1))>0:
+                    mapped = [abu_entry[1], gtf_entry[1]]
+                    mapped = pd.DataFrame(pd.concat(mapped, axis=0)).transpose()
+                    mapped.to_csv(outfile_path, sep="\t", mode="a", header=None)
+
+    data = pd.read_csv(outfile_path, header=None, sep="\t").drop(0, axis=1)
+    data.columns = mapped.columns
+    data.to_csv(outfile_path, mode="w", sep="\t", index=None)
+    return data
+
+def annotate_regions_highmem(abundance: pd.DataFrame, gtf: pd.DataFrame,
+                             outfile_path: str="./out.tsv",
+                             hide_progress: bool=True):
     """
     Annotate region of genome with identifiers. The output format is line-based
     and multi-mapping is possible, for example if a region of interest contains
@@ -88,27 +147,59 @@ def annotate_regions(abundance: pd.DataFrame, gtf: pd.DataFrame,
         FEATURE_1      1123         3402   ...     FOO2  ...
         FEATURE_2       110          149   ...      BAR  ...
     """
-    # TODO: need to optimise, change strategy of matching overlaps?
-    for abu_entry in tqdm(abundance.iterrows(), total=abundance.shape[0],
-                          desc="Annotating genomic regions",
-                          disable=hide_progress):
-        abu_start = abu_entry[1]["start_region"]
-        abu_end = abu_entry[1]["end_region"]
-        abu_chr = abu_entry[1]["region_names"]
+    # create indices corresponding for reverse mapping later on
+    gtf["index"] = gtf["seqnames"] + "_" + gtf["start"].astype(str) + "_" + \
+                   gtf["end"].astype(str)
+    gtf.set_index("index", inplace=True)
+    abundance.set_index("index", inplace=True)
 
-        for gtf_entry in gtf.iterrows():
-            gtf_start = gtf_entry[1]["start"]
-            gtf_end = gtf_entry[1]["end"]
-            gtf_chr = gtf_entry[1]["seqnames"]
+    # intervaltree slicing is not inclusive of end point
+    gtf["end"] = gtf["end"] + 1
+    abundance["end_region"] = abundance["end_region"] + 1
 
-            if abu_chr == gtf_chr:
-                if len(range(max(gtf_start,abu_start),min(gtf_end,abu_end)+1))>0:
-                    mapped = [abu_entry[1], gtf_entry[1]]
-                    mapped = pd.DataFrame(pd.concat(mapped, axis=0)).transpose()
-                    mapped.to_csv(outfile_path, sep="\t", mode="a", header=None)
+    # dont want cross-chromosome overlaps, build one intervaltree per chromosome
+    for chr in tqdm(gtf.seqnames.unique(), total=gtf.seqnames.nunique(),
+                    desc="Annotating genomic regions by chromosome",
+                    disable=hide_progress):
+        ref_coords = IntervalTree()
 
+        # index the reference chromosome
+        gtf_chr = gtf[gtf["seqnames"] == chr]
+        for gtf_entry in gtf_chr.iterrows():
+            start = gtf_entry[1]["start"]
+            end = gtf_entry[1]["end"]
+            index = gtf_entry[0]
+            ref_coords[start:end] = index
+
+        # map the coordinates to reference
+        mapped_features = dict()
+        abu_chr = abundance[abundance["region_names"] == chr]
+        for abu_entry in abu_chr.iterrows():
+            start = abu_entry[1]["start_region"]
+            end = abu_entry[1]["end_region"]
+            index = abu_entry[0]
+            features = ref_coords[start:end]
+            # drop any which map to nothing
+            if features:
+                mapped_features[index] = [i[-1] for i in list(features)]
+
+        # use the indices of data and reference to annotate data with references
+        for i, j in mapped_features.items():
+            key = pd.DataFrame(abundance.loc[i]).T
+            val = gtf.loc[j].reset_index()
+            # line based format, multiple annotations can map to the same region
+            region = pd.concat([key]*len(val))
+            val.rename(columns={"index": "index_reference"}, inplace=True)
+            annotated = pd.concat([region.reset_index(), val], axis=1)
+            annotated["end"] = annotated["end"] - 1
+            annotated["end_region"] = annotated["end_region"] - 1
+            # save memory by writing to disk per block, also checkpoints
+            annotated.to_csv(outfile_path, sep="\t", mode="a", header=None)
+
+    # add the headers back in
     data = pd.read_csv(outfile_path, header=None, sep="\t").drop(0, axis=1)
-    data.columns = mapped.columns
+    data.columns = annotated.columns
+    data.sort_values("adj.P.Val", ascending=False, inplace=True)
     data.to_csv(outfile_path, mode="w", sep="\t", index=None)
     return data
 
@@ -125,8 +216,13 @@ def _argument_parser():
                         help="Provide path to genome annotations file.")
     parser.add_argument("-o", "--outfile_path", type=str,
                         help="Provide path to output file.")
+    parser.add_argument("-t", "--threshold", type=float, default=0.05,
+                        help="Filter on adjusted pval threshold (if DEG list).")
     parser.add_argument("-p", "--hide_progress", action="store_true",
                         help="Hide the progress bar.")
+    parser.add_argument("-l", "--lowmem", action="store_true",
+                        help="Very slow annotation but requires less memory. \
+                        Dont use this unless necessary, O(m x n) from O(n)!")
     return parser.parse_args()
 
 def main():
@@ -150,10 +246,25 @@ def main():
         warnings.warn("# Output file exists, overwriting!",category=UserWarning)
         os.remove(outfile_path)
 
-    annotate_regions(filter_regions(load_regions(infile_path),"adj.P.Val",0.05),
-                     load_gtf(gtf_path),
-                     outfile_path,
-                     hide_progress=args.hide_progress)
+    print("# Reproduce by running this command:")
+    print(" ".join(["python annotate_regions.py", infile_path, gtf_path,
+                    "-o", outfile_path, "-t", str(args.threshold), "-p"]))
+
+    data = load_regions(infile_path)
+
+    if args.threshold:
+        data = filter_regions(data, "adj.P.Val", args.threshold)
+
+    if args.lowmem is False:
+        annotate_regions_highmem(data,
+                                 load_gtf(gtf_path),
+                                 outfile_path,
+                                 hide_progress=args.hide_progress)
+    else:
+        annotate_regions_lowmem(data,
+                                load_gtf(gtf_path),
+                                outfile_path,
+                                hide_progress=args.hide_progress)
 
 if __name__ == "__main__":
     main()
