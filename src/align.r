b2e6ada18a0086518eebd3dc18d2e9f0b646e553
Tyrone Chen <tyrone.chen@monash.edu>
2020-11-13 13:03:36 +1100

    Expand parameters passed to featureCounts

--- src/align.r
+++ src/align.r
@@ -17,19 +17,27 @@ parse_argv <- function() {
                       in column InputFile and OutputFile! If paired-end reads, \
                       include column InputFile2.")
     p <- add_argument(p, "base_name", type="character",
-                      help="Base name for index, eg human_hg19")
+                      help="Base name for index, eg human_hg19.")
     p <- add_argument(p, "reference", type="character",
                       help="Reference genome as a fasta file [fa/fna/fasta]. \
                       This creates the index in that directory.")
+    p <- add_argument(p, "--gtf_annot", type="character",
+                      default=NULL,
+                      help="Custom annotations file in gtf or gtf.gz format.")
+    p <- add_argument(p, "--featuretype", type="character",
+                      default="exon",
+                      help="Annotate this feature from gtf.")
+    p <- add_argument(p, "--featurecount_multimap", flag=TRUE,
+                      help="Include multi-mapping reads in feature counting.")
     p <- add_argument(p, "--counts_path", type="character",
                       default="./counts.tsv",
-                      help="Output file for abundance measurements")
+                      help="Output file for abundance measurements.")
     p <- add_argument(p, "--assembly", type="character", default="hg19",
-                      help="Genome assembly version [hg19, hg38]")
+                      help="Genome assembly version [hg19, hg38].")
     p <- add_argument(p, "--nthreads", type="integer", default=1,
-                      help="Number of threads to use (DEFAULT: 1)")
+                      help="Number of threads to use.")
     p <- add_argument(p, "--mem", type="integer", default=16000,
-                      help="Amount of memory (MB) to use (DEFAULT: 16000)")
+                      help="Amount of memory (MB) to use.")
     argv <- parse_args(p)
     return(argv)
 }
@@ -37,10 +45,13 @@ parse_argv <- function() {
 write_args <- function(args, argpath) {
   args <- paste("Rscript align.r \\ \n  ",
     args$targets, " \\ \n  ", args$base_name, " \\ \n  ",
-    args$reference, " \\ \n  -c", args$counts_path, " \\ \n  -a",
-    args$assembly, " \\ \n  -n", args$nthreads, " \\ \n  -m",
-    args$mem, "\n"
+    args$reference, " \\ \n  -g", args$gtf_annot, " \\ \n  -c",
+    args$counts_path, " \\ \n  -a", args$assembly, "\\ \n -t",
+    args$featuretype, " \\ \n  -n", args$nthreads, " \\ \n  -m", args$mem, "\n"
   )
+  if ( argv$featurecount_multimap == TRUE ) {
+    args <- paste(args, " \\ \n  --featurecount_multimap \n")
+  }
   print(cat(args))
   write(args, sep="", file=argpath)
 }
@@ -69,7 +80,7 @@ main <- function() {
   align(
     index=argv$base_name,
     readfile1=targets$InputFile,
-    readfile2=targets$InputFile2
+    readfile2=targets$InputFile2,
     input_format="gzFASTQ",
     output_format="BAM",
     output_file=targets$OutputFile,
@@ -78,8 +89,25 @@ main <- function() {
     nthreads=argv$nthreads,
   )
 
+  if ( !is.null(targets$InputFile) & !is.null(targets$InputFile2) ) {
+    isPairedEnd <- TRUE
+  } else {isPairedEnd <- FALSE}
+
+  if ( !argv$assembly %in% c("mm9", "mm10", "hg19", "hg38") ) {
+    argv$assembly <- NULL
+  }
+
   # count numbers of reads mapped to NCBI Refseq genes
-  fc <- featureCounts(files=targets$OutputFile, annot.inbuilt=argv$assembly)
+  fc <- featureCounts(
+    files=targets$OutputFile,
+    annot.inbuilt=argv$assembly,
+    annot.ext=argv$gtf_annot,
+    isGTFAnnotationFile=TRUE,
+    GTF.featureType=argv$featuretype,
+    isPairedEnd=isPairedEnd,
+    countMultiMappingReads=argv$featurecount_multimap,
+    nthreads=argv$nthreads
+  )
   setwd(original_dir)
   write.table(fc$counts, sep="\t", quote=F,
     file=paste(argv$outfile_dir, "/", argv$counts_path, ".tsv", sep="")
