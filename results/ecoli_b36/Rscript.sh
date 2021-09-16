3ec78d19e1d2a67a8741b177715d068a97bf3346
Tyrone Chen <tyrone.chen@monash.edu>
2021-09-16 17:32:27 +0800

    Add first pass ecoli b36 results

---
+++ results/ecoli_b36/Rscript.sh
@@ -0,0 +1,36 @@
+Rscript run_pipeline.R \
+   Rscript.sh  \
+   --tune_off FALSE \
+   --mini_run FALSE \
+   --opts NA \
+   --json NA \
+   --classes ../data/sepsis/targets.tsv \
+   --classes_secondary NA \
+   --dropna_classes FALSE \
+   --dropna_prop 0 \
+   --data ../data/sepsis/Metabolomics.tsv ../data/sepsis/Proteomics.tsv ../data/sepsis/Transcriptomics.tsv \
+   --data_names metabolome proteome transcriptome \
+   --force_unique FALSE \
+   --mappings NA \
+   --ncpus 1 \
+   --diablocomp 4 \
+   --linkage 0.1 \
+   --diablo_keepx 5 10 15 30 \
+   --icomp 0 \
+   --zero_as_na TRUE \
+   --replace_missing TRUE \
+   --pcomp 10 \
+   --plsdacomp 2 \
+   --splsdacomp 3 \
+   --splsda_keepx 5 10 15 \
+   --dist_plsda mahalanobis.dist \
+   --dist_splsda mahalanobis.dist \
+   --dist_diablo mahalanobis.dist \
+   --cross_val Mfold \
+   --cross_val_nrepeat 10 \
+   --cross_val_folds 2 \
+   --contrib max \
+   --corr_cutoff 0.95 \
+   --outfile_dir ../results/ecoli_b36/ \
+   --rdata RData.RData \
+   --plot Rplots.pdf
---
+++ src/multiomics_ecoli_b36.sh
@@ -0,0 +1,28 @@
+Rscript run_pipeline.R \
+   --classes ../data/sepsis/targets.tsv \
+   --corr_cutoff 0.95 \
+   --cross_val Mfold \
+   --cross_val_folds 2 \
+   --cross_val_nrepeat 10 \
+   --dropna_classes FALSE \
+   --dropna_prop 0 \
+   --data ../data/sepsis/Metabolomics.tsv ../data/sepsis/Proteomics.tsv ../data/sepsis/Transcriptomics.tsv \
+   --data_names metabolome proteome transcriptome \
+   --force_unique FALSE \
+   --ncpus 1 \
+   --diablocomp 4 \
+   --linkage 0.1 \
+   --diablo_keepx 5 10 15 30 \
+   --icomp 0 \
+   --pcomp 10 \
+   --plsdacomp 2 \
+   --splsdacomp 3 \
+   --splsda_keepx 5 10 15 \
+   --dist_plsda mahalanobis.dist \
+   --dist_splsda mahalanobis.dist \
+   --dist_diablo mahalanobis.dist \
+   --contrib max \
+   --outfile_dir ../results/ecoli_b36/ \
+   --rdata RData.RData \
+   --plot Rplots.pdf \
+   --args Rscript.sh
