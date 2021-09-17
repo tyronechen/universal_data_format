b39177c4f1acd250889d78bc4072e3fc994fd498
Tyrone Chen <tyrone.chen@monash.edu>
2021-09-17 18:01:48 +0800

    Add first pass kvariicola AJ292

---
+++ results/Kvariicola_AJ292/Rscript.sh
@@ -0,0 +1,36 @@
+Rscript run_pipeline.R \
+   Rscript.sh  \
+   --tune_off FALSE \
+   --mini_run FALSE \
+   --opts NA \
+   --json NA \
+   --classes ../data/sepsis/Kvariicola_AJ292/targets.tsv \
+   --classes_secondary NA \
+   --dropna_classes FALSE \
+   --dropna_prop 0 \
+   --data ../data/sepsis/Kvariicola_AJ292/Metabolomics.tsv ../data/sepsis/Kvariicola_AJ292/Proteomics.tsv ../data/sepsis/Kvariicola_AJ292/Transcriptomics.tsv \
+   --data_names metabolome proteome transcriptome \
+   --force_unique FALSE \
+   --mappings NA \
+   --ncpus 6 \
+   --diablocomp 4 \
+   --linkage 0.1 \
+   --diablo_keepx 5 10 15 30 \
+   --icomp 0 \
+   --zero_as_na TRUE \
+   --replace_missing TRUE \
+   --pcomp 10 \
+   --plsdacomp 4 \
+   --splsdacomp 4 \
+   --splsda_keepx 5 10 15 30 \
+   --dist_plsda mahalanobis.dist \
+   --dist_splsda mahalanobis.dist \
+   --dist_diablo mahalanobis.dist \
+   --cross_val Mfold \
+   --cross_val_nrepeat 24 \
+   --cross_val_folds 6 \
+   --contrib max \
+   --corr_cutoff 0.95 \
+   --outfile_dir ../results/Kvariicola_AJ292/ \
+   --rdata RData.RData \
+   --plot Rplots.pdf
